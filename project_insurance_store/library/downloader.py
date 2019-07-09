aaf4c361f0dbde62b7bbee6e716ffe983cd8e0ab
Shan Zhong <zhongshanyes@gmail.com>
2019-07-09 19:52:43 +0800

    Shan

---
+++ project_insurance_store/library/downloader.py
@@ -0,0 +1,17 @@
+import urllib
+
+def getFile(url,path_name,file_name):
+    file_name = path_name + file_name + '.pdf'
+    u = urllib.request.urlopen(url)
+    f = open(file_name, 'wb')
+
+    block_sz = 8192
+    while True:
+        buffer = u.read(block_sz)
+        if not buffer:
+            break
+
+        f.write(buffer)
+    f.close()
+    print ("Sucessful to download" + " " + file_name)
+    
